f3b8de3ada6c6ee61889625591d3944722324a09
Côme Girod <come.girod@epitech.eu>
2025-06-04 13:57:42 +0200

    [FIX] class roi

--- roi.py
+++ roi.py
@@ -1,6 +1,17 @@
 class Roi:
     def __init__(self, couleur, x, y):
         self.couleur = couleur
-        self.mouvement = 1
         self.x = x
         self.y = y
+
+    def deplacement_valide(self, x_cible, y_cible, plateau):
+        if abs(x_cible - self.x) <= 1 and abs(y_cible - self.y) <= 1:
+            return True
+        return False
+
+    def deplacer(self, x_cible, y_cible, plateau):
+        if self.deplacement_valide(x_cible, y_cible, plateau):
+            self.x = x_cible
+            self.y = y_cible
+        else:
+            print("invalid")
